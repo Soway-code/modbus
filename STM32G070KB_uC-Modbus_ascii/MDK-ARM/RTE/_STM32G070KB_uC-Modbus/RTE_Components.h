f21e61ae09b09b4889233381cfeeb4bbcae88ca4
macanlin <893204847@qq.com>
2020-09-18 09:23:12 +0800

    Upload code

---
+++ STM32G070KB_uC-Modbus_ascii/MDK-ARM/RTE/_STM32G070KB_uC-Modbus/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32G070KB_uC-Modbus' 
+ * Target:  'STM32G070KB_uC-Modbus' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32g0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
