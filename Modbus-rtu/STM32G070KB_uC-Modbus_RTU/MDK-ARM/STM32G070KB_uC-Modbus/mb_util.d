7b176e935b7f9c4c7d494de4ba5bb7368862d9c0
macanlin <893204847@qq.com>
2020-09-18 11:45:39 +0800

    Upload code

---
+++ Modbus-rtu/STM32G070KB_uC-Modbus_RTU/MDK-ARM/STM32G070KB_uC-Modbus/mb_util.d
@@ -0,0 +1,5 @@
+stm32g070kb_uc-modbus\mb_util.o: ..\Modbus\Source\MB_UTIL.C
+stm32g070kb_uc-modbus\mb_util.o: ..\Modbus\Source\mb.h
+stm32g070kb_uc-modbus\mb_util.o: ..\Modbus\uC-CPU\cpu.h
+stm32g070kb_uc-modbus\mb_util.o: ..\Modbus\Cfg\mb_cfg.h
+stm32g070kb_uc-modbus\mb_util.o: ..\Modbus\Source\mb_def.h
